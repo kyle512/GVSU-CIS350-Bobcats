b7f50944608e0d44e528c432c08794a5f1daf8f7
pokey136 <pbernreu@gmail.com>
2023-10-30 14:56:31 -0400

    back button

--- src/Godot_Hero/selection.gd
+++ src/Godot_Hero/selection.gd
@@ -4,3 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _on_rock_all_night_pressed():
 	get_tree().change_scene_to_file("res://game.tscn")
+	
+func _on_back():
+	get_tree().change_scene_to_file("res://menu.tscn")
+	
