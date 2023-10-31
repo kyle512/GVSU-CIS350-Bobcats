2802b972c27b7ad8b498d39e81bb8532f5634760
Jack <jackbohan12@gmail.com>
2023-10-30 23:44:42 -0400

    Added hover audio for song selection

--- src/Godot_Hero/selection.gd
+++ src/Godot_Hero/selection.gd
@@ -8,3 +8,27 @@ func _on_rock_all_night_pressed():
 func _on_back():
 	get_tree().change_scene_to_file("res://menu.tscn")
 	
+func _on_even_flow_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+	
+func _on_take_me_out_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+	
+	
+func _on_rock_and_roll_all_nite_mouse_entered():
+	$rockAndRollAllNiteAudio.play()
+	
+func _on_rock_and_roll_all_nite_mouse_exited():
+	$rockAndRollAllNiteAudio.stop()
+	
+func _on_even_flow_mouse_entered():
+	$evenFlowAudio.play()
+
+func _on_even_flow_mouse_exited():
+	$evenFlowAudio.stop()
+
+func _on_take_me_out_mouse_entered():
+	$takeMeOutAudio.play()
+
+func _on_take_me_out_mouse_exited():
+	$takeMeOutAudio.stop()
