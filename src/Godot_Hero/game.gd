46562b698fc10506a10f8db1ab8bb574af497275
pokey136 <pbernreu@gmail.com>
2023-11-04 15:06:32 -0400

    notes with spawn

--- src/Godot_Hero/game.gd
+++ src/Godot_Hero/game.gd
@@ -1,7 +1,49 @@
 extends Node2D
 
+const Gnote = preload("res://move_green.tscn")
+const Rnote = preload("res://move_red.tscn")
+const Bnote = preload("res://move_blue.tscn")
+const Ynote = preload("res://move_yellow.tscn")
+const Onote = preload("res://move_oarnge.tscn")
+
+var RNG = RandomNumberGenerator.new()
+
 func _process(delta):
+	
 	pass
+	
+	
 
 
-
+func _on_timer_timeout():
+	$Timer.start()
+	
+	RNG.randomize()
+	var random = RNG.randi_range(0,5)
+	
+	
+	if random == 1:
+		var green = Gnote.instantiate()
+		add_child(green)
+		green.position = $Spawn/Marker2D_Green.position
+		
+	if random == 2:
+		var red = Rnote.instantiate()
+		add_child(red)
+		red.position = $Spawn/Marker2D_Red.position
+	
+	if random == 3:
+		var blue = Bnote.instantiate()
+		add_child(blue)
+		blue.position = $Spawn/Marker2D_Blue.position
+	
+	if random == 4:
+		var yellow = Ynote.instantiate()
+		add_child(yellow)
+		yellow.position = $Spawn/Marker2D_Yellow.position
+		
+	if random == 5:
+		var orange = Onote.instantiate()
+		add_child(orange)
+		orange.position = $Spawn/Marker2D_Orange.position
+	
--- src/Godot_Hero/idle_red.gd
+++ src/Godot_Hero/idle_red.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
+
+
 func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	Global.sensor_R = 1
+	
 
 
 func _on_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
