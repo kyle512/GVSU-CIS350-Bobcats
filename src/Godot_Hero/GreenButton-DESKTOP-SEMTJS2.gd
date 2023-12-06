4ae45a49095955d1bcceef8454f78b866a5c93f4
Kyle Scott <kylewscott512@gmail.com>
2023-12-05 19:06:31 -0500

    added flames

--- src/Godot_Hero/GreenButton-DESKTOP-SEMTJS2.gd
+++
@@ -1,7 +0,0 @@
-extends TextureButton
-
-func _input(event):
-	if event is InputEventKey and event.scancode == KEY_Y and event.pressed:
-		# Simulate the button press
-		if not is_pressed():
-			press()
